6a40c2caca367a04098221990c91573f6cb69c43
umiar <umiariffah@gmail.com>
2024-05-28 16:34:18 +0800

    refining UI: add tableView

--- src/concurrentapplication/ResultPageController.java
+++ src/concurrentapplication/ResultPageController.java
@@ -2,10 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package concurrentapplication;
 
 import java.io.IOException;
@@ -15,10 +11,15 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.text.Text;
 
 /**
@@ -29,32 +30,42 @@ import javafx.scene.text.Text;
 public class ResultPageController implements Initializable {
 
     @FXML
-    private TextArea wordListText1;
+    private TableView<Word> tableView1;
     @FXML
     private Text time1;
     @FXML
-    private TextArea wordListText2;
+    private TableView<Word> tableView2;
     @FXML
     private Text time2;
     @FXML
     private Text time3;
     @FXML
-    private TextArea wordListText3;
+    private TableView<Word> tableView3;
     @FXML
     private Label os;
     @FXML
     private Label textsize;
-    
-    private String wordsDisplay1 = "";
-    private String wordsDisplay2 = "";
-    private String wordsDisplay3 = "";
 
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        setWordList1(InputPageController.wordList1, InputPageController.qayleefAlgo.getTime() + "");
-        setWordList2(InputPageController.wordList2, InputPageController.jsAlgo.getTime() + "");
-        setWordList3(InputPageController.wordList3, InputPageController.jasonAlgo.getTime() + "");
+        // Populate TableView for Algorithm 1
+        setWordList(tableView1, InputPageController.wordList1);
+
+        // Populate TableView for Algorithm 2
+        setWordList(tableView2, InputPageController.wordList2);
+
+        // Populate TableView for Algorithm 3
+        setWordList(tableView3, InputPageController.wordList3);
+        
+        //Time taken for every algorithm
+        time1.setText("Time taken: " + InputPageController.qayleefAlgo.getTime() + "ms");
+        time2.setText("Time taken: " + InputPageController.jsAlgo.getTime() + "ms");
+        time3.setText("Time taken: " + InputPageController.jasonAlgo.getTime() + "ms");
+        
         
         String osName = System.getProperty("os.name");
         String osVersion = System.getProperty("os.version");
@@ -70,44 +81,31 @@ public class ResultPageController implements Initializable {
         }
     }    
     
-    public void setWordList1(ArrayList<Word> wordList, String time) {
-        wordListText1.setText(formatWordList(wordList));
-        time1.setText("Time taken: " + time + "ms");
-    }
+    public void setWordList(TableView<Word> tableView, ArrayList<Word> wordList) {
+    // Clear existing data
+    tableView.getItems().clear();
+
+    // Create observable list for table data
+    ObservableList<Word> data = FXCollections.observableArrayList(wordList);
+
+    // Set cell value factories for table columns
+    TableColumn<Word, String> wordColumn = new TableColumn<>("Word");
+    wordColumn.setCellValueFactory(cellData -> {
+        // Here we directly access the word field of the Word object
+        return new SimpleStringProperty(cellData.getValue().getWord());
+    });
+
+    TableColumn<Word, Integer> frequencyColumn = new TableColumn<>("Frequency");
+    frequencyColumn.setCellValueFactory(cellData -> {
+        // Here we directly access the frequency field of the Word object
+        return new SimpleIntegerProperty(cellData.getValue().getFrequency()).asObject();
+    });
+
+    tableView.getColumns().clear();
+    tableView.getColumns().addAll(wordColumn, frequencyColumn);
+
+    // Set table data
+    tableView.setItems(data);
+}    
     
-    public void setWordList2(ArrayList<Word> wordList, String time) {
-        wordListText2.setText(formatWordList(wordList));
-        time2.setText("Time taken: " + time + "ms");
-    }
-    
-    public void setWordList3(ArrayList<Word> wordList, String time) {
-        wordListText3.setText(formatWordList(wordList));
-        time3.setText("Time taken: " + time + "ms");
-    }
-
-    private String formatWordList(ArrayList<Word> wordList) {
-        // Determine the maximum word length
-        int maxWordLength = 0;
-        for (Word word : wordList) {
-            maxWordLength = Math.max(maxWordLength, word.getWord().length());
-        }
-
-        // Add extra space between word and frequency
-        int extraSpace = 5;
-        int padding = maxWordLength + extraSpace;
-
-        // Create the table header
-        StringBuilder sb = new StringBuilder();
-        String headerFormat = "%-" + padding + "s%s%n";
-        sb.append(String.format(headerFormat, "Word", "Frequency"));
-        sb.append("-".repeat(padding + 10)).append("\n");
-
-        // Add the word list
-        String rowFormat = "%-" + padding + "s%d%n";
-        for (Word word : wordList) {
-            sb.append(String.format(rowFormat, word.getWord(), word.getFrequency()));
-        }
-
-        return sb.toString();
-    }
 }
